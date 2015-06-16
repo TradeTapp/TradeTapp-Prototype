c9db31f0016225496c0250f2b5e3f480284f5103
Jim Blouin <blouinj@gmail.com>
2015-06-16 08:51:35 -0400

    Moved around the form code to different scss files and created a start to the grid framework using Datatables

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,11 @@ from website import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^grid$', views.grid, name='grid'),
         url(r'^form$', views.form, name='form'),
+        url(r'^form2$', views.form2, name='form2'),
+        url(r'^form3$', views.form3, name='form3'),
+        url(r'^form4$', views.form4, name='form4'),
+        url(r'^form5$', views.form5, name='form5'),
+        url(r'^form6$', views.form6, name='form6'),
 ]
--- website/views.py
+++ website/views.py
@@ -7,9 +7,32 @@ def index(request):
     context = RequestContext(request)
     return render_to_response('index.html',{}, context)
 
+def grid(request):
+    context = RequestContext(request)
+    return render_to_response('grid.html',{}, context)
+
 def form(request):
     context = RequestContext(request)
     return render_to_response('form.html',{}, context)
 
+def form2(request):
+    context = RequestContext(request)
+    return render_to_response('form2.html',{}, context)
+
+def form3(request):
+    context = RequestContext(request)
+    return render_to_response('form3.html',{}, context)
+
+def form4(request):
+    context = RequestContext(request)
+    return render_to_response('form4.html',{}, context)
+
+def form5(request):
+    context = RequestContext(request)
+    return render_to_response('form5.html',{}, context)
+
+def form6(request):
+    context = RequestContext(request)
+    return render_to_response('form6.html',{}, context)
 
 # Create your views here.
