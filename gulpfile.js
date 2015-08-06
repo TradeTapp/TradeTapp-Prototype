a409b84b9ca8787ce62c1db06f69ad94be1681a0
Neil Tan <neil@neiltan.net>
2015-08-06 01:37:08 -0500

    prevents the stop of server when there is an error, still need to have it resume once error has been fixed

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 // Include Our Plugins
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber'); // handle error and "broken pipes"
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -19,7 +20,9 @@ gulp.task('lint', function() {
 // Compile Our Sass
 gulp.task('sass', function() {
     return gulp.src('static/css/app.scss')
+        .pipe(plumber()) // prevent sass from breaking on error.
         .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('static/css'));
 });
 
